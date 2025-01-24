b17d2584bfdda76b3c5ebf3f96ff72b068782e4b
kks940104 <kks940104@gmail.com>
2025-01-24 15:00:24 +0900

    초기업로드

---
+++ cardservice/src/test/java/org/anonymous/CardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anonymous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
